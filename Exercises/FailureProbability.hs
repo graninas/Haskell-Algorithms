6a2f10fc9f9f18cea6d1993c95b2c8e8ffdf9648
Alexandr Granin <graninas@gmail.com>
2012-01-31 17:01:02 +1000

    Haskell-Algorithms FailureProbability
    Working version. Too slow on problem3. Should be rewritten and optimised.
    (Fix)

--- Exercises/FailureProbability.hs
+++ Exercises/FailureProbability.hs
@@ -1,87 +1,119 @@
 module Main where
 
+-- | Подключение модулей работы со списками (Data.List) и с монадами (Control.Monad).
+--   Модули подключаются под именами L и M соответственно.
+--   Из модуля Control.Monad берется только функция guard.
 import qualified Data.List as L
-import qualified Control.Monad as M
+import qualified Control.Monad as M (guard)
 
-type ListData = (Char, Float)
+-- | Используемые в программе типы данных.
+type DeviceData      = (Char, Float)   -- | Данные по устройству: символ и вероятность выхода из строя.
+type DevicesData     = [DeviceData]    -- | Список данных по устройствам.
+type FailureCostData = [Float]         -- | Суммы убытков каждого из отказов.
+type ProblemData     = (DevicesData, FailureCostData) -- | Данные задачи.
+type Damages         = [Float]                        -- | Убыточность устройств.
 
-devices :: [ListData]
-devices = [ ('A', 0.37)
-		  , ('B', 0.43)
-		  , ('C', 0.2)
-		  ]
+-- | Данные по устройствам.
+devices1, devices2 :: DevicesData
+devices1 = [ ('A', 0.37)
+           , ('B', 0.43)
+           , ('C', 0.2) ]
 
-failureCost :: [Float]
-failureCost = [ 20, 20, 30, 30 ]
+devices2 = [ ('A', 0.17)
+           , ('B', 0.13)
+           , ('C', 0.20)
+           , ('D', 0.09)
+           , ('E', 0.40)
+           , ('F', 0.01) ]
 
-variations = L.permutations devices
+devices3 = [ ('A', 0.10)
+           , ('B', 0.11)
+           , ('C', 0.05)
+           , ('D', 0.04)
+           , ('E', 0.14)
+           , ('F', 0.10)
+           , ('G', 0.10)
+           , ('H', 0.09)
+           , ('I', 0.15)
+           , ('J', 0.12) ]
+
+-- | Суммы убытков из-за отказов.
+failureCost1, failureCost2 :: FailureCostData
+failureCost1 = [20, 20, 30, 30]
+failureCost2 = [ 8, 12, 20, 60]
+failureCost3 = [ 1,  3, 11, 34, 51]
+
+-- | Полные данные по примерам 1, 2, 3.
+problem1, problem2, problem3 :: ProblemData
+problem1 = (devices1, failureCost1)
+problem2 = (devices2, failureCost2)
+problem3 = (devices3, failureCost3)
+
+
+-- | Возвращает список из символьных имен устройств.
+deviceNames :: DevicesData -> [Char]
+deviceNames = map fst
 
 sums [] = 0
 sums ((_,pa):aas) = pa + sums aas
 
-sums' :: [ListData] -> Float
-sums' = foldr (\(_, p) -> (p +)) 0
+variantProbability xs = f xs (sums xs)
+  where
+    f [] _ = ("", 1)
+    f ((dev, p):xs) sum = let
+        (devs, probs) = f xs (sum - p)
+        prob          = (p / sum)
+        in (dev : devs, prob * probs)
 
-variantProbability [] = ("", 1)
-variantProbability ((dev, p):xs) = let
-	(devs, probs) = variantProbability xs
-	prob          = p / (p + sums xs)
-	in (dev : devs, prob * probs)
+variantProbabilities = map variantProbability . L.permutations
+
+cell xs c@(dev, pos) = (c, sum devProbs)
+  where
+    devProbs = do
+        varProb <- xs
+        M.guard (pos `elem` (L.elemIndices dev . fst $ varProb))
+        return (snd varProb)
+
+devicePositionProb devices colsCount devName = do
+    col <- [0..colsCount - 1]
+    return (cell (variantProbabilities devices) (devName, col))
+
+damage (devices, failCosts) devName = let
+    colsCount            = length failCosts
+    devPosProbs          = devicePositionProb devices colsCount devName
+    producted            = zipWith (\x y -> snd x * y) devPosProbs failCosts
+    in sum producted
+
+probabilityTable (devices, failCosts)  = do
+    devName <- deviceNames devices
+    let colsCount = (length failCosts) - 1
+    return (devicePositionProb devices colsCount devName)
+
+damages :: ProblemData -> Damages
+damages problemData = do
+    devName <- deviceNames (fst problemData)
+    return (damage problemData devName)
+
+
+main = do
+    let results = damages problem3
+    putStrLn . show $ results
+
+
+sums' = foldr (\(_, p) -> (p +)) 0
 
 variantProbability' [] = ("", 1)
 variantProbability' d@((dev, p) : xs) = let
-	(devs, probs) = variantProbability' xs
-	prob          = (p / sums d)
-	in (dev : devs, prob * probs)
+    (devs, probs) = variantProbability' xs
+    prob          = (p / sums d)
+    in (dev : devs, prob * probs)
 
-variantProbability'' xs = f xs (sums xs)
-  where
-	f [] _ = ("", 1)
-	f ((dev, p):xs) sum = let
-		(devs, probs) = f xs (sum - p)
-		prob          = (p / sum)
-		in (dev : devs, prob * probs)
+variantProbability'' [] = ("", 1)
+variantProbability'' ((dev, p):xs) = let
+    (devs, probs) = variantProbability'' xs
+    prob          = p / (p + sums xs)
+    in (dev : devs, prob * probs)
 
-variantProbabilities   = map variantProbability   variations
-variantProbabilities'  = map variantProbability'  variations
-variantProbabilities'' = map variantProbability'' variations -- Немного другой результат!
-
--- [("ABC",0.2525397),("BAC",0.27912283),("CBA",0.10750001),("BCA",0.1508772),("CAB",9.25e-2),("ACB",0.117460325)]
--- [("ABC",0.2525397),("BAC",0.27912283),("CBA",0.10750001),("BCA",0.1508772),("CAB",9.25e-2),("ACB",0.117460325)]
--- [("ABC",0.25253972),("BAC",0.27912286),("CBA",0.10750001),("BCA",0.1508772),("CAB",9.25e-2),("ACB",0.117460325)]
-
-
-cell xs c@(dev, pos) = (c, sum devProbs')
-  where
-	devProbs = [snd varProb | varProb <- xs, pos `elem` (L.elemIndices dev . fst $ varProb)]
-	devProbs' = do
-		varProb <- xs
-		M.guard (pos `elem` (L.elemIndices dev . fst $ varProb))
-		return (snd varProb)
-
-row dev = do
-	col <- [0..(length failureCost) - 1]
-	return (cell variantProbabilities (dev, col))
-
-table = do
-	dev <- map fst devices
-	return (row dev)
-
-damage dev = let
-	zipped = zipWith (\x y -> snd x * y) (row dev) failureCost
-	in sum zipped
-
-damages = map damage (map fst devices)
-
-{-
-failureCost = [ 20, 20, 30, 30 ]
-
-	[ (('A',0),0.37),(('A',1),0.37162283),(('A',2),0.2583772)
-	, (('B',0),0.43),(('B',1),0.3600397), (('B',2),0.20996033)
-	, (('C',0),0.20000002),(('C',1),0.26833752),(('C',2),0.5316625)
-	]
-	
-zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-
-zipWith generalises zip by zipping with the function given as the first argument, instead of a tupling function. For example, zipWith (+) is applied to two lists to produce the list of corresponding sums. 
--}
+cell' xs c@(dev, pos) = let
+    devProbs = [snd varProb | varProb <- xs, pos `elem` (L.elemIndices dev . fst $ varProb)]
+    in (c, sum devProbs)
