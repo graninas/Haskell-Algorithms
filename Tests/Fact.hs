bd4a0bc5bd84f7215a918a6a9f573a19a108e36f
Granin Alexandr <graninas@gmail.com>
2012-05-15 22:33:53 +0700

    - Minor changes.

--- Tests/Fact.hs
+++ Tests/Fact.hs
@@ -1,5 +1,15 @@
-fact 0 = 1
-fact n = fact (n-1) * n
+fact1 0 = 1
+fact1 n = fact1 (n-1) * n
 
-fact' n | n == 0 = 1
-        | otherwise = fact' (n-1) * n
+fact2 n | n == 0 = 1
+        | otherwise = fact2 (n-1) * n
+
+fact3 n = case n == 0 of
+    True  -> 1
+    False -> fact3 (n-1) * n
+
+fact4 n = product [1..n]
+
+fact5 n = if n == 0 
+          then 1
+          else n * fact5 (n-1)
