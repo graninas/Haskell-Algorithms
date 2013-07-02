ef51fb620b7edc92f780218d471ab6d50fb9cd96
Granin Alexandr <graninas@gmail.com>
2013-07-03 01:01:33 +0700

    - Removed unused stuff. - Added Task2.hs.

--- Tests/Robotics/Task2.hs
+++ Tests/Robotics/Task2.hs
@@ -1,26 +1,28 @@
-{- Дано конечное ограниченное поле 5х5, в каждой ячейке которого имеется одна стрелка: вверх, вниз, вправо или влево.
-   На позицию (x1, y1) помещается робот Тинк.
-   На позицию (x2, y2) помещается роботиха Кики.
-   Тинк движется по полю. Стрелка, на которую он зашел, указывает Кики, куда ей двигаться.
-   Если движение невозможно (за край поля выйти нельзя), Кики остается на месте.
-   
-   Задача: найти все решения для n шагов, когда Кики встречается с Тинком.
+{- 
 -}
 
 module Main where
 
 import Data.List (nub, partition, sort, group)
 
+type Field = [[Char]]
+
 u = 'u'
 d = 'd'
 r = 'r'
 l = 'l'
 
-field = [ [u, l, u, r, l]
-        , [u, d, d, r, r]
-        , [l, r, r, l, l]
-        , [d, r, u, u, u]
-        , [d, d, r, l, u] ]
+field1 = [ [d, r, u, r, l]
+         , [l, u, d, l, r]
+         , [l, d, d, r, r]
+         , [d, r, u, l, u]
+         , [u, d, r, r, d] ]
+
+field2 = [ [d, r, u, r, l]
+         , [r, u, d, l, r]
+         , [r, r, d, r, r]
+         , [d, r, u, l, u]
+         , [u, d, r, r, d] ]
 
 type Position = (Int, Int)
 type Positions = [Position]
@@ -46,9 +48,9 @@ moveKiki (kx, ky) 'd' = (normalize $ kx + 1, ky)
 moveKiki (kx, ky) 'l' = (kx, normalize $ ky - 1)
 moveKiki (kx, ky) 'r' = (kx, normalize $ ky + 1)
 
-move :: Dislocation -> Position -> Dislocation
-move (tink, kiki) newTink@(tx, ty) = let
-    kikiAction = (field !! tx) !! ty
+move :: Field -> Dislocation -> Position -> Dislocation
+move f (tink, kiki) newTink@(tx, ty) = let
+    kikiAction = (f !! tx) !! ty
     newKiki = moveKiki kiki kikiAction
     in (newTink, newKiki)
 
@@ -65,14 +67,14 @@ data SolutionTree = SL Dislocation
   deriving (Show, Read, Eq)
 
 
-solve :: Int -> Dislocation -> SolutionTree
-solve 1 d@(tink, kiki) | tink == kiki = SL d
-                       | otherwise = L d
-solve ttl d@(tink, kiki) | tink == kiki = SL d
-                         | otherwise = B d $ map (solve newTtl) nextDislocations
+solve :: Field -> Int -> Dislocation -> SolutionTree
+solve f 1 d@(tink, kiki) | tink == kiki = SL d
+                         | otherwise = L d
+solve f ttl d@(tink, kiki) | tink == kiki = SL d
+                           | otherwise = B d $ map (solve f newTtl) nextDislocations
   where
     newTtl = ttl - 1
-    nextDislocations = map (move d) (possibleMoves tink)
+    nextDislocations = map (move f d) (possibleMoves tink)
 
 type Path = [Dislocation]
 type Paths = [Path]
@@ -89,13 +91,15 @@ foldSuccesses (B d branches) = case folded branches of
     folded bs = concat $ map foldSuccesses bs
 
 -- Показать статистику решений для глубины n
-try n = let ss = foldSuccesses (solve n startPositions)
-            pathLengths = map length ss
-            groupedPathLengths = group . sort $ pathLengths
-            pathLengthStats = map (\gpls -> (head gpls, length gpls)) groupedPathLengths
-        in (length ss, pathLengthStats)
+try f n = let ss = foldSuccesses (solve f n startPositions)
+              pathLengths = map length ss
+              groupedPathLengths = group . sort $ pathLengths
+              pathLengthStats = map (\gpls -> (head gpls, length gpls)) groupedPathLengths
+          in (length ss, pathLengthStats)
 
 -- Показать решения длиной k для глубины n
-solutions k n = let ss = foldSuccesses (solve n startPositions)
-                    kLengths = filter ((== k) . length) ss
-                in kLengths
+solutions f k n = let ss = foldSuccesses (solve f n startPositions)
+                      kLengths = filter ((== k) . length) ss
+                  in kLengths
+
+goodSolution = solutions field1 6 10
