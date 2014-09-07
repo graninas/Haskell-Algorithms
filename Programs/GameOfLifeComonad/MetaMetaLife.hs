35f9818cb2fa4f04743d7d2d7e4a03848feb9b0b
Alexander Granin <graninas@gmail.com>
2014-09-08 02:05:38 +0700

    Stable configuration detected.

--- Programs/GameOfLifeComonad/MetaMetaLife.hs
+++ Programs/GameOfLifeComonad/MetaMetaLife.hs
@@ -13,8 +13,8 @@ data Cell =  Dead | Alive
 type MetaCell = (Int, Cell)
 type MetaMetaCell = (Int, MetaCell)
 
-factor'' = 2
-factor' = 1
+factor'' = 3
+factor' = 2
 
 isAlive (_, (_, c)) = c == Alive
 zeroCellCreator c = (0, (0, c))
@@ -31,9 +31,9 @@ rule'' u
 
 rule' :: Universe2 MetaMetaCell -> MetaMetaCell
 rule' u
-    | nc <  (factor' - factModifier'' - factModifier'')   = (factModifier'', (0, snd . snd $ rule u))
+    | nc <  (factor' - factModifier' - factModifier'')   = (factModifier'', (0, snd . snd $ rule u))
     | nc == factor'                                       = (factModifier'', (1,  snd . snd $ rule u))
-    | nc >  (factor' + factModifier'' + factModifier'')   = (factModifier'', (-1,  snd . snd $ rule u))
+    | nc >  (factor' + factModifier' + factModifier'')   = (factModifier'', (-1,  snd . snd $ rule u))
     | otherwise                                           = (factModifier'', (0,  snd . snd $ rule u))
   where
     old@(factModifier'', (factModifier', c)) = extract u
