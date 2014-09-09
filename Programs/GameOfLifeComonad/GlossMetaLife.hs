369c821127630879d37e915ddb96cc0a57b1b3b0
Alexander Granin <graninas@gmail.com>
2014-09-09 09:16:39 +0700

    Tunning rules.

--- Programs/GameOfLifeComonad/GlossMetaLife.hs
+++ Programs/GameOfLifeComonad/GlossMetaLife.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Universe
 import MetaLife
+import SampleCells
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
@@ -35,28 +36,27 @@ pickCellRadius c | c == dead = deadRadius
 
 pickFactorRadius f = 7.0 + (fromIntegral f)
 
-renderCell'' (f'', (f', c)) = Pictures [
-    Color black $ Circle (pickFactorRadius f''),
-    Color (pickColor f')  $ ThickCircle thickness (pickCellRadius c)
+renderCell :: MetaCell -> Picture
+renderCell ((df1', df2', df3'), (df1, df2, df3), c) = Pictures [
+    Color black $ Circle (pickFactorRadius df2'),
+    Color (pickColor df1')  $ ThickCircle thickness (pickCellRadius c)
     ]
-   
 
-renderLife :: Universe2 MetaMetaCell -> Picture
+renderLife :: Universe2 MetaCell -> Picture
 renderLife = toPicture . toList2
 
-toPicture :: [[MetaMetaCell]] -> Picture
+toPicture :: [[MetaCell]] -> Picture
 toPicture picss = (foldr1 mappend . foldr1 mappend) $ map toPicture' (zip [screenXShift..] picss)
   where
     toPicture' (j, pics) = map (transCellX j) (zip [screenYShift..] pics)
 
-transCellX :: Int -> (Int, MetaMetaCell) -> Picture
-transCellX j (i, mc) = Translate ((fromIntegral i) * side) ((fromIntegral j) * side) (renderCell'' mc)
+transCellX :: Int -> (Int, MetaCell) -> Picture
+transCellX j (i, mc) = Translate ((fromIntegral i) * side) ((fromIntegral j) * side) (renderCell mc)
 
+stepLife :: MetaFactor -> a -> Float -> Universe2 MetaCell -> Universe2 MetaCell
+stepLife mf _ _ = stepLifeUniverse mf
 
-stepLife :: MetaFactor -> a -> Float -> Universe2 MetaMetaCell -> Universe2 MetaMetaCell
-stepLife mf _ _ = stepLifeUniverse'' mf
-
-type Universes = (Int, Int, [Universe2 MetaMetaCell])
+type Universes = (Int, Int, [Universe2 MetaCell])
 
 stepUniverses' :: Universes -> Universes
 stepUniverses' (cur, end, us) | cur >= end = (cur, end, us)
@@ -64,15 +64,18 @@ stepUniverses' (cur, end, us) | cur >= end = (cur, end, us)
 stepUniverses :: a -> Float -> Universes -> Universes
 stepUniverses _ _ = stepUniverses'
 
+initialModel :: Int -> Universe2 MetaCell
+initialModel s = fromList2 s zeroCell metaCells
+
+mkIteration ti s mf = (0, ti, universes)
+  where universes = iterate (stepLifeUniverse mf) (initialModel s)
+
 renderUniverses :: Universes -> Picture
 renderUniverses (cur, _, us) = renderLife . head . drop cur $ us
 
 eventHandler :: Event -> Universes -> Universes
 eventHandler (EventKey (SpecialKey KeySpace) Down _ _) us = stepUniverses' us
 eventHandler _ us = us
-
-mkIteration ti s mf = (0, ti, universes)
-  where universes = iterate (stepLifeUniverse'' mf) (initialModel s)
 
 run Play ti ips s mf = play windowCfg white ips
                              (mkIteration ti s mf)
@@ -84,16 +87,20 @@ run Simulate ti ips s mf = simulate windowCfg white ips
                              renderUniverses
                              stepUniverses
 
-
-
 main = do
     confData <- readFile configFile
     let conf = filter (\l -> head l /= '#') . filter (not . null) . lines $ confData
     if null conf
         then putStrLn $ "Invalid configuration file: " ++ configFile
         else do
-            let [f'', f', sizeStr, totalItersStr, itersPerSecStr, modeStr] = words . head $ conf
-            let mf          = (read f'', read f') :: (Int, Int)
+            let [f1Str, f2Str, f3Str, f4Str, f5Str
+                 , ra1Str, ra2Str, ra3Str
+                 , sizeStr
+                 , totalItersStr
+                 , itersPerSecStr
+                 , modeStr] = words . head $ conf
+            let mf = (read f1Str, read f2Str, read f3Str, read f4Str, read f5Str
+                     , read ra1Str, read ra2Str, read ra3Str) :: MetaFactor
             let s           = (read sizeStr) :: Int
             let mode        = read modeStr :: Mode
             let totalIters  = read totalItersStr :: Int
--- Programs/GameOfLifeComonad/MetaLife.hs
+++ Programs/GameOfLifeComonad/MetaLife.hs
@@ -14,44 +14,71 @@ type Cell = Int
 dead  = 0
 alive = 1
 
-type MetaCell = (Int, Cell)
-type MetaMetaCell = (Int, MetaCell)
-type MetaFactor = (Int, Int)
+data RuleArea = Ring1 | Ring2 | Ring3
+  deriving (Show, Read)
 
-isAlive (_, (_, c)) = c == alive
-zeroCellCreator c = (0, (0, c))
-zeroCell :: MetaMetaCell
+-- dfOtL for df Old Threshold Left  Limit
+-- dfMt for df Middle Threshold Limit
+-- dftR for df Threshold Right
+-- Classic GOL:
+
+
+type DfOtL = Int
+type DfMt = Int
+type DftR = Int
+type RuleModifiers   = (DfOtL, DfMt, DftR)
+type DfModifiers     = (DfOtL, DfMt, DftR)
+type MetaCell        = (DfModifiers, RuleModifiers, Cell)
+type MetaFactor      = (Int, Int, Int, Int, Int, RuleArea, RuleArea, RuleArea)
+
+zeroModifiers       = (0, 0, 0)
+zeroCellCreator c   = (zeroModifiers, zeroModifiers, c)
+zeroCell :: MetaCell
 zeroCell = zeroCellCreator dead
 
-rule'' :: MetaFactor -> Universe2 MetaMetaCell -> MetaMetaCell
-rule'' mf@(f'', f') u
-    | nc == f''   = (0,  new)
-    | nc <  f''   = (1,  new)
-    | otherwise   = (-1, new)
-  where
-    new = snd $ rule' mf u
-    nc = length $ filter isAlive (parNeighbours neighbours'' u)
+stepLifeUniverse mf = (=>> (rule'' mf))
+isAlive :: MetaCell -> Bool
+isAlive (_, _, c) = c == alive
 
-rule' :: MetaFactor -> Universe2 MetaMetaCell -> MetaMetaCell
-rule' mf@(f'', f') u
-    | nc <  (f' - factModifier' - factModifier'')  = (factModifier'', (0,  new))
-    | nc == f'                                      = (factModifier'', (1,  new))
-    | nc >  (f' + factModifier' + factModifier'')  = (factModifier'', (-1, new))
-    | otherwise                                     = (factModifier'', (0,  new))
+rule'' :: MetaFactor -> Universe2 MetaCell -> MetaCell
+rule'' mf@(_, _, _, f4, f5, _, _, r3) u
+--    | nc < f4      = ((dfOtL', dfMt', dftR'), oldMods, new)
+--    | nc < f4 + f5 = ((1, 1, 1),              oldMods, new)
+    | otherwise    = ((1, 0, 1),           oldMods, new)
   where
-    old@(factModifier'', (factModifier', c)) = extract u
-    new = snd . snd $ rule mf u
-    nc = length $ filter isAlive (parNeighbours neighbours' u)
+    old@( (dfOtL', dfMt', dftR')
+        , oldMods
+        , c) = extract u
+    (_, _, new) = rule' mf u
+    ruleArea = pickRuleArea r3
+    nc       = length $ filter isAlive (parNeighbours ruleArea u)
 
-rule :: MetaFactor -> Universe2 MetaMetaCell -> MetaMetaCell
-rule mf@(f'', f') u
-    | nc == 2   = old
-    | nc == 3   = (f'', (f', alive))
-    | otherwise = (f'', (f', dead))
+rule' :: MetaFactor -> Universe2 MetaCell -> MetaCell
+rule' mf@(f1, f2, f3, _, _, _, r2, _) u
+    | nc < f1           = (oldDfMods, (dfOtL' + dfOtL, dfMt', dftR' + dftR),     new)
+    | nc < f1 + f2      = (oldDfMods, (dfOtL, dfMt,  dftR),                 new)
+    | otherwise         = (oldDfMods, (dfOtL' - dfOtL, dfMt', dftR' - dftR), new)
+  where
+    old@( oldDfMods@(dfOtL', dfMt', dftR')
+        , oldMods  @(dfOtL,  dfMt,  dftR)
+        , c) = extract u
+    (_, _, new) = rule mf u
+    ruleArea = pickRuleArea r2
+    nc = length $ filter isAlive (parNeighbours ruleArea u)
+
+rule :: MetaFactor -> Universe2 MetaCell -> MetaCell
+rule mf@(_, _, _, _, _, r1, _, _) u
+    | nc >= (2 - dfOtL) && nc < (3 + dfMt) = old
+    | nc >= (3 + dfMt)  && nc < (4 + dftR) = (oldDf', oldDf, alive)
+    | otherwise                            = (oldDf', oldDf, dead)
     where
-        old@(f'', (f', c)) = extract u
-        nc = length $ filter isAlive (parNeighbours neighbours u)
+        ruleArea = pickRuleArea r1
+        old@(oldDf', oldDf@(dfOtL, dfMt, dftR), c) = extract u
+        nc = length $ filter isAlive (parNeighbours ruleArea u)
 
+pickRuleArea Ring1 = neighbours
+pickRuleArea Ring2 = neighbours'
+pickRuleArea Ring3 = neighbours''
 
 parNeighbours :: NFData a => ((Universe2 a) -> [a]) -> (Universe2 a) -> [a]
 parNeighbours ns u = runEval $ parList rpar (force $ ns u)
@@ -92,48 +119,5 @@ neighbours u =
     , nearest3 . extract . right
     ] >>= ($ getUniverse2 u)
 
-initialModel :: Int -> Universe2 MetaMetaCell
-initialModel s = fromList2 s zeroCell metaCells
 
-stepLifeUniverse'' mf = (=>> (rule mf))
-
-metaCells = map (map zeroCellCreator) cellsGlider
-
-cells' = [[alive, alive, dead, alive, alive]]
-
-cells'' = [[alive, alive, alive]]
-
-cellsGlider = [ [ dead, alive,  dead]
-              , [alive,  dead,  dead]
-              , [alive, alive, alive] ]
-
-cells''' = [ [alive, dead, alive, alive, alive, alive ]
-        , [dead, alive, alive, alive, dead, alive ]
-        , [alive, dead, dead, alive, alive, alive ]
-        , [dead, alive, dead, dead, alive, dead ]
-        , [alive, dead, dead, alive, alive, dead ]
-        , [dead, alive, alive, dead, dead, alive ]
-        , [alive, dead, alive, alive, alive, alive ]
-        , [dead, alive, alive, dead, alive, alive ]
-        , [alive, dead, alive, dead, alive, alive ]
-        , [dead, alive, dead, dead, alive, dead ]
-        , [alive, dead, alive, alive, dead, dead ]
-        , [dead, alive, alive, dead, alive, alive ]
-        , [alive, dead, dead, alive, dead, dead ]
-        ]
-
-testLine  = [ alive : replicate 28 dead ++ [alive]]
-testBlock = [ replicate 14 dead ++ [alive, alive] ++ replicate 14 dead]
-fillers n = replicate n (replicate 30 dead)
-
-testCells =  testLine
-      ++ fillers 6
-      ++ testLine
-      ++ fillers 6
-      ++ testBlock
-      ++ testBlock
-      ++ fillers 6
-      ++ testLine
-      ++ fillers 6
-      ++ testLine
 
