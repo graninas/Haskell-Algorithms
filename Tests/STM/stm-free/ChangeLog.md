59dd590d226115061c959c23c3da59cba64ac2e6
Alexander Granin <graninas@gmail.com>
2018-02-22 02:38:38 +0700

    STM moved to a separate repo.

--- Tests/STM/stm-free/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for stm-free
-
-## Unreleased changes
--- Tests/STM/stm-free/src/Control/Concurrent/STM/Free/TVar.hs
+++
@@ -1,5 +0,0 @@
-module Control.Concurrent.STM.Free.TVar where
-
-import Control.Concurrent.STM.Free.Internal.Types
-
-data TVar a = TVar TVarId
