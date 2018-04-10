c3db5ea237abcf10bfa94b09c250f01cf37e21f7
Alexander Granin <graninas@gmail.com>
2018-04-11 01:41:30 +0700

    Some test application.

--- Tests/TransitionTree/transition-tree/app/Main.hs
+++ Tests/TransitionTree/transition-tree/app/Main.hs
@@ -5,32 +5,50 @@ import qualified Data.ByteString.Char8 as BS
 
 import           Lib
 
+
+
 printLevel :: String -> Lang ()
 printLevel = printS
 
 travel3Tree :: Tree () ()
 travel3Tree = tree $
-  with (printLevel "3")
+  with location3
     <~> on "forward" (leaf (return ()))
 
 travel2Tree :: Tree () ()
 travel2Tree = tree $
-  with (printLevel "2")
+  with location2
     <~> on "forward" travel3Tree
 
 travel1Tree :: Tree () ()
 travel1Tree = tree $
-  with (printLevel "1")
+  with location1
     <~> on "forward" travel2Tree
+
+location1 :: Lang ()
+location1 = location "You are standing on front of a house."
+
+location2 :: Lang ()
+location2 = location "Another location."
+
+location3 :: Lang ()
+location3 = location "Location #3."
+
+location :: String -> Lang ()
+location description = do
+  printLevel description
+  -- getInput
 
 interpretLang :: LangF s -> IO s
 interpretLang (PrintS s next)  = print s >> return next
+-- interpretLang (GetInput nextF) = nextF <$> getLine
 interpretLang (GetInput nextF) = error "Not implemented."
 
 runLang :: Lang s -> IO (Event, s)
 runLang l = do
-  r <- foldFree interpretLang l
-  return ("forward", r)
+  result <- foldFree interpretLang l
+  input  <- getLine
+  return (input, result)
 
 isBkEv event = event == "back"
 
