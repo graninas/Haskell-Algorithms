877571edca651ee3c1944f53ae0e222e5f2eade2
Alexander Granin <graninas@gmail.com>
2019-08-18 17:26:25 +0700

    Recording-Replaying Mechanism Showcase WIP.

--- Tests/RR/src/Lib.hs
+++ Tests/RR/src/Lib.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE DeriveAnyClass            #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE FunctionalDependencies    #-}
+{-# LANGUAGE TypeSynonymInstances      #-}
+{-# LANGUAGE FlexibleInstances         #-}
 
 module Lib
     ( someFunc
@@ -7,23 +12,45 @@ module Lib
 
 import           Control.Monad      (unless, when)
 import           Control.Monad.Free
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BSL
 import           Data.UUID          (toString)
 import           Data.UUID.V4       (nextRandom)
+import           Data.Aeson         (ToJSON, FromJSON, encode, decode)
+import           Data.Proxy         (Proxy(..))
+import           Data.Text          (Text)
+import           GHC.Generics       (Generic)
 
--- data FlowF next
---     = GenerateGUID
---         (Playback.RRItemDict Playback.GenerateGUIDEntry String)
---         (String -> next)
---
---     | RunIO (forall eff. BackendAff eff s) (s -> next)
---
---     | Log String s
---         (Playback.RRItemDict Playback.LogEntry UnitEx)
---         (UnitEx -> next)
+data GenerateGUIDEntry = GenerateGUIDEntry
+  { guid :: String
+  }
+  deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
+
+mkGenerateGUIDEntry :: String -> GenerateGUIDEntry
+mkGenerateGUIDEntry = GenerateGUIDEntry
+
+data RunIOEntry = RunIOEntry
+  { jsonResult :: String
+  }
+  deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
+
+data LogInfoEntry = LogInfoEntry
+  { message :: String
+  }
+  deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
+
+instance RRItem GenerateGUIDEntry String where
+  toRecordingEntry rrItem idx = RecordingEntry idx "GenerateGUIDEntry"
+    $ BS.unpack $ BSL.toStrict $ encode rrItem
+  fromRecordingEntry (RecordingEntry _ _ payload) =
+    decode $ BSL.fromStrict $ BS.pack payload
+  getTag _ = "GenerateGUIDEntry"
+  parseRRItem (GenerateGUIDEntry guid) = Just guid
+
 
 data FlowF next where
   GenerateGUID :: (String -> next) -> FlowF next
-  RunIO :: IO s -> (s -> next) -> FlowF next
+  RunIO :: (ToJSON s, FromJSON s) => IO s -> (s -> next) -> FlowF next
   LogInfo :: String -> (() -> next) -> FlowF next
 
 instance Functor FlowF where
@@ -36,7 +63,7 @@ type Flow a = Free FlowF a
 generateGUID :: Flow String
 generateGUID = liftF $ GenerateGUID id
 
-runIO :: IO s -> Flow s
+runIO :: (ToJSON s, FromJSON s) => IO s -> Flow s
 runIO ioAct = liftF $ RunIO ioAct id
 
 logInfo :: String -> Flow ()
@@ -63,7 +90,67 @@ interpretFlowFTest (GenerateGUID next) = pure $ next "111"
 interpretFlowFTest (RunIO ioAct next)  = error "IO not supported in tests."
 interpretFlowFTest (LogInfo msg next)  = pure $ next ()
 
+data RecorderRuntime = RecorderRuntime
+data PlayerRuntime = PlayerRuntime
 
+data Runtime
+  = RegularMode
+  | RecordingMode RecorderRuntime
+  | ReplayingMode PlayerRuntime
+
+
+type EntryName = String
+type EntryPayload = String
+type EntryIndex = Int
+data RecordingEntry = RecordingEntry EntryIndex EntryName EntryPayload
+
+-- type RecordingEntries = [RecordingEntry]
+-- newtype Recording = Recording RecordingEntries
+
+class (Eq rrItem, ToJSON rrItem, FromJSON rrItem)
+  => RRItem rrItem native
+   | rrItem -> native where
+  toRecordingEntry   :: rrItem -> Int -> RecordingEntry
+  fromRecordingEntry :: RecordingEntry -> Maybe rrItem
+  getTag             :: Proxy rrItem -> String
+  parseRRItem        :: rrItem -> Maybe native
+
+
+record = undefined
+replay = undefined
+
+withRunMode
+  :: RRItem rrItem native
+  => Runtime
+  -> (native -> rrItem)
+  -> IO native
+  -> IO native
+withRunMode RegularMode _ act = act
+withRunMode (RecordingMode recorderRt) rrItem act
+  = record recorderRt rrItem act
+withRunMode (ReplayingMode playerRt) rrItem act
+  = replay playerRt rrItem act
+
+interpretFlowFRR :: Runtime -> FlowF a -> IO a
+interpretFlowFRR rt (GenerateGUID next) = do
+  res <- withRunMode rt
+    mkGenerateGUIDEntry
+    (toString <$> nextRandom)
+  pure $ next res
+interpretFlowFRR rt (RunIO ioAct next)  = error "IO not supported in tests."
+interpretFlowFRR rt (LogInfo msg next)  = pure $ next ()
+
+-- initDB :: String -> DB.DBConfig -> Maybe DB.Connection
+-- initDB dbName cfg = do
+--   mbConn <- runIO $ DB.initDatabase dbName cfg
+--   when (isJust mbConn) $ logInfo $ "Successfully initialized."
+--   pure mbConn
+
+-- scenario :: Flow Int
+-- scenario = do
+--   students <- runDBQuery "SELECT * FROM students"
+--   when (null students) $ logInfo "No records found."
+--   pure $ length students
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
